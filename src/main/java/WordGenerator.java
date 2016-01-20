aad571164c8a672d53b44913be855315c758bbaa
nolan <nolan.barth@gmail.com>
2016-01-20 11:24:36 -0500

    Capitalized first letters of words              because why not

--- src/main/java/WordGenerator.java
+++ src/main/java/WordGenerator.java
@@ -4,7 +4,7 @@ public class WordGenerator {
     String word;
     Random rand = new Random();
     public String generate(String s){
-        word = s;
+        word = s.toUpperCase();
         int length = rand.nextInt(9);
 
         for (int i = 0; i < length; i++ ) {
